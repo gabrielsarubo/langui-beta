4d0e5412b988ea20bc4647439d4c05d4d09e0ed4
Gabriel <gabrielsarubo@hotmail.com>
2021-05-14 22:04:56 -0300

    Create Profile page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Blog from "./pages/Blog";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
@@ -12,10 +13,11 @@ function App() {
       <div className="App">
         <Route path='/signin' component={SignIn} />
         <Route path='/signup' component={SignUp} />
-        {/* <Header /> */}
+        <Header />
         <Route exact path='/' component={Home} />
         <Route path='/blog' component={Blog} />
-        {/* <Navbar /> */}
+        <Route path='/profile' component={Profile} />
+        <Navbar />
       </div>
     </BrowserRouter>
   );
---
+++ src/pages/Profile/index.js
@@ -0,0 +1,32 @@
+const Profile = () => {
+  const profileData = {
+    name: 'Gabriel Sousa',
+    email: 'gabrielsaubo@hotmail.com',
+    password: 'abcd1234'
+  }
+
+  const { name, email, password } = profileData
+
+  return (
+    <div className="Profile container">
+      <h1 className="h3 mb-4"><i class="bi bi-person-fill"></i>&nbsp;Perfil</h1>
+      <div className="mb-2">
+        <label className="form-label">Nome</label>
+        <input type="name" id="name" className="form-control" value={name} placeholder="Seu nome" readOnly disabled />
+      </div>
+      <div className="mb-2">
+        <label className="form-label">Email</label>
+        <input type="email" id="email" className="form-control" value={email} placeholder="Seu email" readOnly disabled />
+      </div>
+      <div className="mb-4">
+        <label className="form-label">Senha</label>
+        <input type="password" id="password" className="form-control" value={password} placeholder="Sua senha" readOnly disabled />
+      </div>
+      <hr />
+      <button className="btn btn-dark">
+      <i class="bi bi-box-arrow-left"></i>&nbsp;&nbsp;Desconectar</button>
+    </div>
+  );
+}
+
+export default Profile;
