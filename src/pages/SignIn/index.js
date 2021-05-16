294db199bfc2b472baa64f2fc68901420362e167
Gabriel <gabrielsarubo@hotmail.com>
2021-05-16 17:44:52 -0300

    Turn SignIn, SignUp into UI comp

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,55 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './index.css';
 import Logo from '../../assets/brand/langui-logo.svg';
 
-class SignIn extends Component {
-  state = {
-    email: '',
-    password: ''
-  }
+const SignIn = () => {
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
 
-  handleChange = e => {
-    this.setState({
-      [e.target.id]: e.target.value
-    })
-  }
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault()
 
-    console.log(this.state)
+    console.log(email, password);
   }
 
-  render() {
-    return (
-      // Container for the entire sign component
-      <div className="SignIn">
-        <form onSubmit={this.handleSubmit}>
-          <div className="mb-4">
-            <img src={Logo} alt="" />
-          </div>
-          <h1 className="h3 mb-4">Entre na sua conta</h1>
-          <div className="mb-2">
-            <label htmlFor="email" className="form-label">Email</label>
-            <input type="email" id="email" className="form-control" onChange={this.handleChange} placeholder="Seu email" required />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="form-label">Senha</label>
-            <input type="password" id="password" className="form-control" onChange={this.handleChange} placeholder="Sua senha" required />
-          </div>
-          <button type="submit" className="btn btn-primary w-100" 
-            disabled={(!(this.state.email !== '' && this.state.password !== '')) || (false)}>
-            Entrar
+  return (
+    // Container for the entire sign component
+    <div className="SignIn">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <img src={Logo} alt="" />
+        </div>
+        <h1 className="h3 mb-4">Entre na sua conta</h1>
+        <div className="mb-2">
+          <label htmlFor="email" className="form-label">Email</label>
+          <input type="email" id="email" className="form-control" onChange={e => setEmail(e.target.value)} placeholder="Seu email" required />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password" className="form-label">Senha</label>
+          <input type="password" id="password" className="form-control" onChange={e => setPassword(e.target.value)} placeholder="Sua senha" required />
+        </div>
+        <button type="submit" className="btn btn-primary w-100"
+          disabled={(!(email !== '' && password !== '')) || (false)}>
+          Entrar
           </button>
-          <p className="mt-3 mb-0 text-muted text-center">
-            Não tem conta?&nbsp;
+        <p className="mt-3 mb-0 text-muted text-center">
+          Não tem conta?&nbsp;
             <Link to='/signup'>Crie uma agora</Link>
-          </p>
-        </form>
-      </div>
-    );
-  }
+        </p>
+      </form>
+    </div>
+  );
 }
 
 export default SignIn;
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,60 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './index.css';
 import Logo from '../../assets/brand/langui-logo.svg';
 
-class SignUp extends Component {
-  state = {
-    name: '',
-    email: '',
-    password: ''
-  }
+const SignUp = () => {
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
 
-  handleChange = e => {
-    this.setState({
-      [e.target.id]: e.target.value
-    })
-  }
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault()
 
-    console.log(this.state)
+    console.log(name, email, password)
   }
 
-  render() {
-    return (
-      // Container for the entire sign component
-      <div className="SignUp">
-        <form onSubmit={this.handleSubmit}>
-          <div className="mb-4">
-            <img src={Logo} alt="" />
-          </div>
-          <h1 className="h3 mb-4">Crie uma conta</h1>
-          <div className="mb-2">
-            <label htmlFor="name" className="form-label">Nome</label>
-            <input type="name" id="name" className="form-control" onChange={this.handleChange} placeholder="Seu nome" required />
-          </div>
-          <div className="mb-2">
-            <label htmlFor="email" className="form-label">Email</label>
-            <input type="email" id="email" className="form-control" onChange={this.handleChange} placeholder="Seu email" required />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="form-label">Senha</label>
-            <input type="password" id="password" className="form-control" onChange={this.handleChange} placeholder="Sua senha" required />
-          </div>
-          <button type="submit" className="btn btn-primary w-100" 
-            disabled={(!(this.state.email !== '' && this.state.email !== '' && this.state.password !== '')) || (false)}>
-            Criar Conta
+  return (
+    // Container for the entire sign component
+    <div className="SignUp">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <img src={Logo} alt="" />
+        </div>
+        <h1 className="h3 mb-4">Crie uma conta</h1>
+        <div className="mb-2">
+          <label htmlFor="name" className="form-label">Nome</label>
+          <input type="name" id="name" className="form-control" onChange={e => { setName(e.target.value) }} placeholder="Seu nome" required />
+        </div>
+        <div className="mb-2">
+          <label htmlFor="email" className="form-label">Email</label>
+          <input type="email" id="email" className="form-control" onChange={e => { setEmail(e.target.value) }} placeholder="Seu email" required />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password" className="form-label">Senha</label>
+          <input type="password" id="password" className="form-control" onChange={e => { setPassword(e.target.value) }} placeholder="Sua senha" required />
+        </div>
+        <button type="submit" className="btn btn-primary w-100"
+          disabled={(!(name !== '' && email !== '' && password !== '')) || (false)}>
+          Criar Conta
           </button>
-          <p className="mt-3 mb-0 text-muted text-center">
-            Já tem conta?&nbsp;
+        <p className="mt-3 mb-0 text-muted text-center">
+          Já tem conta?&nbsp;
             <Link to='/signin'>Entre agora</Link>
-          </p>
-        </form>
-      </div>
-    );
-  }
+        </p>
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
