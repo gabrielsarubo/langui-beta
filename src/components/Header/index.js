2d11bbeed8ed5ef953e9815f4f7255e0d51cdf8c
Gabriel <gabrielsarubo@hotmail.com>
2021-05-17 02:33:23 -0300

    Update styles for Header comp

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,10 +6,9 @@ const Header = () => {
     <header className="mainHeader">
       <div className="container-fluid">
         {/* Container for the brand logo and name */}
-        <div className="d-flex">
-          <img src={Logo} alt="Langui Logo" width="36" height="36" />
-          <div className="logotipo">Langui</div>
-        </div>
+        <div className="logotipo">Langui</div>
+        {/* Logo Symbol */}
+        <img src={Logo} className="logo-symbol" alt="Langui Logo" width="36" height="36" />
         {/* Container for the streak counter */}
         <div className="streak-container">
           <i className="bi bi-lightning-charge-fill"></i>&nbsp;7
